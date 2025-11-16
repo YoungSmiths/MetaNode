86c93d90b5bac7e0d71d33f0ad63eef082e2785e
ShiYang <young_smith@163.com>
2025-11-16 21:11:39 +0800

    流程控制：只出现一次的数字

--- go/homework/part1/singleNumber.go
+++ go/homework/part1/singleNumber.go
@@ -6,15 +6,37 @@ import "fmt"
 *136. 只出现一次的数字
  */
 func singleNumber(nums []int) int {
-	var single int = 0
+	var single = 0
 	for _, value := range nums {
 		single ^= value
 	}
 	return single
+}
 
+func singleNumber2(nums []int) int {
+	var single = 0
+	for i := 0; i < len(nums); i++ {
+		single ^= nums[i]
+	}
+	return single
+}
+
+func singleNumber3(nums []int) int {
+	var single = 0
+	var index = 0
+	for {
+		if index >= len(nums) {
+			break
+		}
+		single ^= nums[index]
+		index++
+	}
+	return single
 }
 
 func main() {
 	nums := []int{4, 1, 2, 1, 2}
 	fmt.Println(singleNumber(nums))
+	fmt.Println(singleNumber2(nums))
+	fmt.Println(singleNumber3(nums))
 }
