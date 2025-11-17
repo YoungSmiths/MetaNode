178ba605f4231f4d39e36f298f6d7e387ddb9949
YoungSmiths <youngsmiths@users.noreply.github.com>
2025-11-17 20:47:27 +0800

    指针

---
+++ go/prictice/pointer.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+	pointer2()
+}
+
+func pointer1() {
+	var p *string
+	fmt.Println(p)
+	fmt.Printf("p的值是%s/n", p)
+	if p != nil {
+		fmt.Println("非空")
+	} else {
+		fmt.Println("空值")
+	}
+}
+
+func pointer2() {
+	var a *int
+	*a = 100
+	fmt.Println(*a)
+
+	var b map[string]int
+	b["测试"] = 100
+	fmt.Println(b)
+}
